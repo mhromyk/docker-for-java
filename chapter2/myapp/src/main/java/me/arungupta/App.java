ae5e391934bb3444b0febd8a48b0c9c5efd7f720
maksym hromyk <maksym_hromyk@epam.com>
2018-07-27 17:25:33 +0200

    add some test files into chapter2 folder

--- chapter2/myapp/src/main/java/me/arungupta/App.java
+++ chapter2/myapp/src/main/java/me/arungupta/App.java
@@ -5,6 +5,6 @@ package me.arungupta;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Howdy World!" );
     }
 }
